ab628ea5e5d19a25bbbb9d52403ed243f8cbc550
99003785 <sourav.dey@ltts.com>
2021-02-13 00:02:44 +0530

    arithmetic

---
+++ Arithmetic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int add(int,int);
+int sub(int,int);
+int mul(int,int);
+int divide(int,int);
+void main()
+{
+   printf("enter two num:\n");
+   
+}
+
+int add(int a,int b )
+{
+     int sum = a+b;
+    return sum;
+}
