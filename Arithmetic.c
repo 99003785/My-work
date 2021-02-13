2ba725206b235a8d8a117539d53a0e36b7336178
99003785 <sourav.dey@ltts.com>
2021-02-13 10:03:33 +0530

    initial work

---
+++ Arithmetic.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int add(int,int);
+int subtraction(int,int);
+int multi(int,int);
+int divide(int,int);
+void main()
+{
+   printf("enter two num:\n");
+   
+}
+
+int add(int a,int b )
+{
+     int sum = a+b;
+    return sum;
+}
+
+int subtraction(int a,int b)
+{
+    int sub = a-b; 
+}
